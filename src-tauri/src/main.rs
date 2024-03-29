eafa9f70375d281c3ade324b5346a582be1d584d
Stephen Grice <stephengrice@live.com>
2024-03-29 12:44:01 -0400

    Add file picker from PTD

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -20,6 +20,26 @@ fn project_new(handle: tauri::AppHandle, app_state: State<AppState>) {
 #[tauri::command]
 fn project_open(handle: tauri::AppHandle, app_state: State<AppState>) {
     println!("Open project clicked.");
+    let app_state = app_state.0.clone();
+    dialog::FileDialogBuilder::new()
+        .pick_file(move |file_path| {
+            if let Some(path) = file_path {
+                app_state.lock().unwrap().filepath = path.to_str().unwrap().to_string();
+                tauri::WindowBuilder::new(
+                    &handle,
+                    "editor",
+                    tauri::WindowUrl::App("index2.html".into()),
+                )
+                .title("KeyDo - Folder")
+                .build()
+                .unwrap();
+                handle
+                    .get_window("open-project")
+                    .unwrap()
+                    .close()
+                    .expect("Unable to close window");
+            }
+    });
 }
 
 fn main() {
