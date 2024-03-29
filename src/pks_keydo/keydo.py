78755416bd0dd9c55369e4db91f084b34b4647e0
Stephen Grice <stephengrice@live.com>
2024-03-29 12:24:41 -0400

    Try to install qt

--- src/pks_keydo/keydo.py
+++ src/pks_keydo/keydo.py
@@ -1,3 +1,10 @@
+import tkinter
+import webview
+
 
 def main():
     print("Hello world")
+    tk = tkinter.Tk()
+    tk.geometry("800x450")
+    webview.create_window("TEsting", "https://google.com")
+    webview.start()
